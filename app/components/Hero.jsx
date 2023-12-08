3258ccb1ee62eedf65c4ebaa4317a2bfb10dc334
atefeh-karimi <atefehkarimikia0922@gmail.com>
2023-12-09 00:09:01 +0330

    change some style

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,71 +3,26 @@ import { TypeAnimation } from "react-type-animation";
 
 export default function Hero() {
   return (
-    <div className="h-full">
-      <div className="relative h-full isolate">
-        <svg
-          className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
-          aria-hidden="true"
-        >
-          <defs>
-            <pattern
-              id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84"
-              width={200}
-              height={200}
-              x="50%"
-              y={-1}
-              patternUnits="userSpaceOnUse"
-            >
-              <path d="M.5 200V.5H200" fill="none" />
-            </pattern>
-          </defs>
-          <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-            <path
-              d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
-              strokeWidth={0}
+    <div className="container mx-auto">
+      <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
+        <div className="px-6 pt-10 lg:col-span-7 lg:px-0 xl:col-span-6">
+          <div className="max-w-2xl mx-auto lg:mx-0">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">
+              Hello,I'm Atefeh.
+            </h1>
+            <p className="mt-6 mb-2 text-lg leading-8 text-gray-600">
+              Front-End Developer
+            </p>
+            <TypeAnimation
+              sequence={[
+                "As a dedicated front-end enthusiast, I specialize in creating responsive, visually stunning interfaces. My love for UX extends beyond aesthetics — I believe in designing experiences that delight users and enhance usability.",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: "1em", display: "inline-block" }}
+              repeat={Infinity}
             />
-          </svg>
-          <rect
-            width="100%"
-            height="100%"
-            strokeWidth={0}
-            fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)"
-          />
-        </svg>
-        <div
-          className="absolute top-0 right-0 -ml-24 overflow-hidden left-1/2 -z-10 transform-gpu blur-3xl lg:ml-24 xl:ml-48"
-          aria-hidden="true"
-        >
-          <div
-            className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-            style={{
-              clipPath:
-                "polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)",
-            }}
-          />
-        </div>
-      </div>
-      <div className="container relative mx-auto">
-        <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
-          <div className="px-6 pt-10 pb-24 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
-            <div className="max-w-2xl mx-auto lg:mx-0">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">
-                Hello,I'm Atefeh.
-              </h1>
-              <p className="mt-6 mb-2 text-lg leading-8 text-gray-600">
-                Front-End Developer
-              </p>
-              <TypeAnimation
-                sequence={[
-                  "As a dedicated front-end enthusiast, I specialize in creating responsive, visually stunning interfaces. My love for UX extends beyond aesthetics — I believe in designing experiences that delight users and enhance usability.",
-                  1000,
-                ]}
-                wrapper="span"
-                speed={50}
-                style={{ fontSize: "1em", display: "inline-block" }}
-                repeat={Infinity}
-              />
-            </div>
           </div>
         </div>
       </div>
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function Projects() {
-  return <div className="p-6 mx-auto max-w-7xl lg:px-8">Projects</div>;
+  return (
+    <div className="p-6 mx-auto max-w-7xl lg:px-8">
+      <div className="relative bg-white isolate ">
+        <div
+          className="absolute inset-x-0 top-0 flex pt-10 overflow-hidden opacity-25 -z-10 transform-gpu blur-3xl sm:pt-40 xl:justify-end"
+          aria-hidden="true"
+        >
+          <div
+            className="ml-[-20rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[45deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] xl:ml-0 xl:mr-[calc(50%-12rem)]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+      </div>
+      <div>Projects</div>
+    </div>
+  );
 }
